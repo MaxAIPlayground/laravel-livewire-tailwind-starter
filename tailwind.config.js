876c74c19f05436acb289db6091a77a947682e95
Maximilian Patzner <dev@maximilian-patzner.de>
2024-05-16 14:55:25 +0200

    Add daisyui tailwind plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
+import daisyui from "daisyui"
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -19,5 +20,13 @@ export default {
         },
     },
 
-    plugins: [forms, typography],
+    plugins: [/*forms,*/typography, daisyui],
+
+    daisyui: {
+        themes: ["halloween"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+        base: true, // applies background color and foreground color for root element by default
+        styled: true, // include daisyUI colors and design decisions for all components
+        utils: true, // adds responsive and modifier utility classes
+        themeRoot: ":root", // The element that receives theme color CSS variables
+    },
 };
